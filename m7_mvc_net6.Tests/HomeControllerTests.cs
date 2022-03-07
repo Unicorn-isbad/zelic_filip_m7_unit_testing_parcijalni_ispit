d9562ea25425882ed1cba759cf8e9dc87ef769b9
Unicorn-isbad <69872616+Unicorn-isbad@users.noreply.github.com>
2022-03-07 18:50:18 +0100

    Fin

---
+++ m7_mvc_net6.Tests/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using m7_mvc_net6.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m7_mvc_net6.Tests
+{
+    public class HomeControllerTests
+    {
+        ILogger<HomeController> _logger;
+        [Theory, InlineData(500)]
+        public void HomeController_CheckCountValue_Fail(int i)
+        {
+            Assert.Throws<Exception>(() => (new HomeController(_logger).CheckCountValue(i)));
+        }
+        [Theory, InlineData(2)]
+        public void HomeController_CheckCountValue_Success(int i)
+        {
+            Assert.IsType<ViewResult>((new HomeController(_logger).CheckCountValue(i)));
+        }
+    }
+}
---
+++ m7_mvc_net6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace m7_mvc_net6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
